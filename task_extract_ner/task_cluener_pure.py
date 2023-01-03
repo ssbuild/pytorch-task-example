bbef17a6b5bd9e19e73e5f2607fde55de578b698
ssbuild <462304@qq.cn>
2023-01-03 17:27:31 +0800

    pure ner测试中
    Signed-off-by: ssbuild <462304@qq.cn>

---
+++ task_extract_ner/task_cluener_pure.py
@@ -0,0 +1,373 @@
+# -*- coding: utf-8 -*-
+# 测试中...
+import copy
+import json
+import logging
+import typing
+
+import numpy as np
+import torch
+from deep_training.data_helper import DataHelper
+from deep_training.data_helper import ModelArguments, DataArguments, TrainingArguments
+from deep_training.data_helper import load_tokenizer_and_config_with_args
+from deep_training.nlp.metrics.pointer import metric_for_pointer
+from deep_training.nlp.models.pure_model import TransformerForPure,PureModelArguments, extract_lse
+from deep_training.utils.trainer import SimpleModelCheckpoint
+from pytorch_lightning import Trainer
+from torch.utils.data import DataLoader, IterableDataset
+from tqdm import tqdm
+from transformers import HfArgumentParser, BertTokenizer
+
+train_info_args = {
+    'devices': 1,
+    'data_backend': 'memory_raw',
+    'model_type': 'bert',
+    'model_name_or_path': '/data/nlp/pre_models/torch/bert/bert-base-chinese',
+    'tokenizer_name': '/data/nlp/pre_models/torch/bert/bert-base-chinese',
+    'config_name': '/data/nlp/pre_models/torch/bert/bert-base-chinese/config.json',
+    'do_train': True,
+    'do_eval': True,
+    'train_file': '/data/nlp/nlp_train_data/clue/cluener/train.json',
+    'eval_file': '/data/nlp/nlp_train_data/clue/cluener/dev.json',
+    'test_file': '/data/nlp/nlp_train_data/clue/cluener/test.json',
+    'learning_rate': 5e-5,
+    'learning_rate_for_task': 5e-5,
+    'max_epochs': 15,
+    'train_batch_size': 40,
+    'eval_batch_size': 1,
+    'test_batch_size': 1,
+    'adam_epsilon': 1e-8,
+    'gradient_accumulation_steps': 1,
+    'max_grad_norm': 1.0,
+    'weight_decay': 0,
+    'warmup_steps': 0,
+    'output_dir': './output',
+    'train_max_seq_length': 380,
+    'eval_max_seq_length': 512,
+    'test_max_seq_length': 512,
+######pure model args
+    'max_span_length': 50,
+    'head_hidden_dim': 150,
+    'width_embedding_dim': 150,
+}
+
+
+class NN_DataHelper(DataHelper):
+    index = -1
+    eval_labels = []
+
+    # 切分成开始
+    def on_data_ready(self):
+        self.index = -1
+
+    # 切分词
+    def on_data_process(self, data: typing.Any, user_data: tuple):
+        self.index += 1
+
+        tokenizer: BertTokenizer
+        tokenizer, max_seq_length, do_lower_case, label2id, mode = user_data
+        sentence, entities = data
+
+        tokens = list(sentence) if not do_lower_case else list(sentence.lower())
+        input_ids = tokenizer.convert_tokens_to_ids(tokens)
+        if len(input_ids) > max_seq_length - 2:
+            input_ids = input_ids[:max_seq_length - 2]
+        input_ids = [tokenizer.cls_token_id] + input_ids + [tokenizer.sep_token_id]
+        attention_mask = [1] * len(input_ids)
+
+        input_ids = np.asarray(input_ids, dtype=np.int32)
+        attention_mask = np.asarray(attention_mask, dtype=np.int32)
+        seqlen = np.asarray(len(input_ids), dtype=np.int32)
+        labels = []
+        real_label = []
+
+        if entities is not None:
+            for l, s, e in entities:
+                l = label2id[l]
+                real_label.append((l, s, e))
+                s += 1
+                e += 1
+                if s < max_seq_length - 1 and e < max_seq_length - 1:
+                    labels.append((l + 1,s,e))
+
+        labels = np.asarray(labels,dtype=np.int32)
+        pad_len = max_seq_length - len(input_ids)
+        if pad_len > 0:
+            input_ids = np.pad(input_ids, (0, pad_len), 'constant',
+                               constant_values=(tokenizer.pad_token_id, tokenizer.pad_token_id))
+            attention_mask = np.pad(attention_mask, (0, pad_len), 'constant', constant_values=(0, 0))
+        d = {
+            'input_ids': input_ids,
+            'attention_mask': attention_mask,
+            'labels': labels,
+            'seqlen': seqlen,
+        }
+
+        # if self.index < 5:
+        #     print(tokens)
+        #     print(input_ids[:seqlen])
+        #     print(attention_mask[:seqlen])
+        #     print(seqlen)
+
+        if mode == 'eval':
+            self.eval_labels.append(real_label)
+        return d
+
+    # 读取标签
+    def on_get_labels(self, files: typing.List[str]):
+        labels = [
+            'address', 'book', 'company', 'game', 'government', 'movie', 'name', 'organization', 'position', 'scene'
+        ]
+        labels = list(set(labels))
+        labels = sorted(labels)
+        label2id = {label: i for i, label in enumerate(labels)}
+        id2label = {i: label for i, label in enumerate(labels)}
+
+        NN_DataHelper.label2id = label2id
+        NN_DataHelper.id2label = id2label
+        return label2id, id2label
+
+    # 读取文件
+    def on_get_corpus(self, files: typing.List, mode: str):
+        D = []
+        for filename in files:
+            with open(filename, mode='r', encoding='utf-8') as f:
+                lines = f.readlines()
+                for line in lines:
+                    jd = json.loads(line)
+                    if not jd:
+                        continue
+                    # cluener 为 label，  fastlabel 为 entities
+                    entities = jd.get('label', None)
+                    if entities:
+                        entities_label = []
+                        for k, v in entities.items():
+                            pts = [_ for a_ in list(v.values()) for _ in a_]
+                            for pt in pts:
+                                assert pt[0] <= pt[1], ValueError(line, pt)
+                                entities_label.append((k, pt[0], pt[1]))
+                    else:
+                        entities_label = None
+                    D.append((jd['text'], entities_label))
+        return D
+
+    @staticmethod
+    def collate_fn(batch):
+        o = {}
+
+        labels_fake = []
+        for i, b in enumerate(batch):
+            b = copy.copy(b)
+            labels = b.pop('labels',None)
+            labels_fake.append(labels)
+            if i == 0:
+                for k in b:
+                    o[k] = [torch.tensor(b[k])]
+            else:
+                for k in b:
+                    o[k].append(torch.tensor(b[k]))
+        for k in o:
+            o[k] = torch.stack(o[k])
+
+        bs = o['input_ids'].size(0)
+        seqlens = o.pop('seqlen')
+        max_len = torch.max(seqlens)
+
+
+        tokens_len = max_len
+        max_span_length = NN_DataHelper.max_span_length if NN_DataHelper.max_span_length > 0 else tokens_len
+        max_span_length = min(max_span_length,tokens_len)
+
+        labels = []
+        spans = []
+        spans_mask = []
+        has_label = labels_fake[0] is not None
+        #
+        for idx in range(bs):
+            if has_label:
+                labels_map = {tuple(ldata[1:]): ldata[0] for ldata in labels_fake[idx]}
+            seqlen = seqlens[idx]
+            label,span,span_mask = [],[],[]
+            for i in range(tokens_len):
+                for j in range(i,min(i+max_span_length,tokens_len)):
+                    span.append((i,j,len(span)))
+                    span_mask.append(1 if i < seqlen and j < seqlen else 0)
+                    if has_label:
+                        label.append(labels_map.get((i,j),0))
+            if has_label:
+                labels.append(label)
+            spans.append(span)
+            spans_mask.append(span_mask)
+
+        spans = torch.tensor(spans,dtype=torch.int32)
+        spans_mask = torch.tensor(spans_mask,dtype=torch.int32)
+        labels = torch.tensor(labels,dtype=torch.int32)
+
+        o['input_ids'] = o['input_ids'][:, :max_len]
+        o['attention_mask'] = o['attention_mask'][:, :max_len]
+        if 'token_type_ids' in o:
+            o['token_type_ids'] = o['token_type_ids'][:, :max_len]
+
+        o['spans'] = spans
+        o['spans_mask'] = spans_mask
+        if has_label:
+            o['labels'] = labels
+        return o
+
+
+class MyTransformer(TransformerForPure, with_pl=True):
+    def __init__(self, eval_labels, *args, **kwargs):
+        super(MyTransformer, self).__init__(*args, **kwargs)
+        self.model.eval_labels = eval_labels
+        self.eval_labels = eval_labels
+
+
+class MySimpleModelCheckpoint(SimpleModelCheckpoint):
+    def __init__(self, *args, **kwargs):
+        super(MySimpleModelCheckpoint, self).__init__(*args, **kwargs)
+        self.weight_file = './best.pt'
+
+    def on_save_model(
+            self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"
+    ) -> None:
+        pl_module: MyTransformer
+
+        # 当前设备
+        device = torch.device('cuda:{}'.format(trainer.global_rank))
+        eval_datasets = dataHelper.load_dataset(dataHelper.eval_files)
+        eval_datasets = DataLoader(eval_datasets, batch_size=training_args.eval_batch_size,
+                                   collate_fn=dataHelper.collate_fn)
+
+        threshold = 1e-8
+        eval_labels = pl_module.eval_labels
+        config = pl_module.config
+
+        y_preds, y_trues = [], []
+        for i, batch in tqdm(enumerate(eval_datasets), total=len(eval_datasets), desc='evalute'):
+            for k in batch:
+                batch[k] = batch[k].to(device)
+            o = pl_module.validation_step(batch, i)
+
+            logits, _ = o['outputs']
+            y_preds.extend(extract_lse(logits, threshold))
+            bs = len(logits)
+            y_trues.extend(eval_labels[i * bs: (i + 1) * bs])
+
+        f1, str_report = metric_for_pointer(y_trues, y_preds, config.label2id)
+        print(f1)
+        print(str_report)
+
+        best_f1 = self.best.get('f1', -np.inf)
+        print('current', f1, 'best', best_f1)
+        if f1 >= best_f1:
+            self.best['f1'] = f1
+            logging.info('save best {}, {}\n'.format(self.best['f1'], self.weight_file))
+            trainer.save_checkpoint(self.weight_file)
+
+
+if __name__ == '__main__':
+    parser = HfArgumentParser((ModelArguments, TrainingArguments, DataArguments,PureModelArguments))
+    model_args, training_args, data_args,puremodel_args = parser.parse_dict(train_info_args)
+
+    checkpoint_callback = MySimpleModelCheckpoint(monitor='val_f1', every_n_epochs=1)
+    trainer = Trainer(
+        log_every_n_steps=10,
+        callbacks=[checkpoint_callback],
+        max_epochs=training_args.max_epochs,
+        max_steps=training_args.max_steps,
+        accelerator="gpu",
+        devices=data_args.devices,
+        enable_progress_bar=True,
+        default_root_dir=data_args.output_dir,
+        gradient_clip_val=training_args.max_grad_norm,
+        accumulate_grad_batches=training_args.gradient_accumulation_steps,
+        num_sanity_val_steps=0,
+        strategy='ddp' if torch.cuda.device_count() > 1 else None,
+    )
+
+    dataHelper = NN_DataHelper(data_args.data_backend)
+    tokenizer, config, label2id, id2label = load_tokenizer_and_config_with_args(dataHelper, model_args, training_args,
+                                                                                data_args)
+
+    token_fn_args_dict = {
+        'train': (tokenizer, data_args.train_max_seq_length, model_args.do_lower_case, label2id, 'train'),
+        'eval': (tokenizer, data_args.eval_max_seq_length, model_args.do_lower_case, label2id, 'eval'),
+        'test': (tokenizer, data_args.test_max_seq_length, model_args.do_lower_case, label2id, 'test')
+    }
+    #max_span_length
+    NN_DataHelper.max_span_length = puremodel_args.max_span_length
+
+    # 缓存数据集
+    intermediate_name = data_args.intermediate_name + '_{}'.format(0)
+    if data_args.do_train:
+        dataHelper.train_files.append(
+            dataHelper.make_dataset_with_args(data_args.train_file, token_fn_args_dict['train'],
+                                              data_args,
+                                              intermediate_name=intermediate_name, shuffle=True,
+                                              mode='train'))
+    if data_args.do_eval:
+        dataHelper.eval_files.append(dataHelper.make_dataset_with_args(data_args.eval_file, token_fn_args_dict['eval'],
+                                                                       data_args,
+                                                                       intermediate_name=intermediate_name,
+                                                                       shuffle=False,
+                                                                       mode='eval'))
+    if data_args.do_test:
+        dataHelper.test_files.append(dataHelper.make_dataset_with_args(data_args.test_file, token_fn_args_dict['test'],
+                                                                       data_args,
+                                                                       intermediate_name=intermediate_name,
+                                                                       shuffle=False,
+                                                                       mode='test'))
+
+    train_datasets = dataHelper.load_dataset(dataHelper.train_files, shuffle=True, num_processes=trainer.world_size,
+                                             process_index=trainer.global_rank, infinite=True,
+                                             with_record_iterable_dataset=True)
+    if train_datasets is not None:
+        train_datasets = DataLoader(train_datasets, batch_size=training_args.train_batch_size,
+                                    collate_fn=dataHelper.collate_fn,
+                                    shuffle=False if isinstance(train_datasets, IterableDataset) else True)
+
+    model = MyTransformer(dataHelper.eval_labels,puremodel_args=puremodel_args, config=config, model_args=model_args, training_args=training_args)
+
+    if train_datasets is not None:
+        trainer.fit(model, train_dataloaders=train_datasets)
+    else:
+        eval_datasets = dataHelper.load_dataset(dataHelper.eval_files)
+        test_datasets = dataHelper.load_dataset(dataHelper.test_files)
+        if eval_datasets is not None:
+            eval_datasets = DataLoader(eval_datasets, batch_size=training_args.eval_batch_size,
+                                       collate_fn=dataHelper.collate_fn)
+        if test_datasets is not None:
+            test_datasets = DataLoader(test_datasets, batch_size=training_args.test_batch_size,
+                                       collate_fn=dataHelper.collate_fn)
+        if eval_datasets is not None:
+            trainer.validate(model, dataloaders=eval_datasets, ckpt_path='./best.pt')
+
+        if test_datasets is not None:
+            trainer.test(model, dataloaders=test_datasets, ckpt_path='best.pt')
+
+        is_convert_onnx = True
+        # 是否转换模型
+        if is_convert_onnx:
+            input_sample = (
+                torch.ones(size=(1, 128), dtype=torch.int64),
+                torch.ones(size=(1, 128), dtype=torch.int64),
+            )
+            model.eval()
+            model.to('cuda')
+            input_names = ["input_ids", "attention_mask"]
+            out_names = ["pred_ids"]
+
+            model = MyTransformer.load_from_checkpoint('./best.pt',puremodel_args=puremodel_args, config=config, model_args=model_args,
+                                                       training_args=training_args)
+            model.to_onnx('./best.onnx',
+                          input_sample=input_sample,
+                          verbose=True,
+                          opset_version=14,
+                          do_constant_folding=True,
+                          input_names=input_names,
+                          output_names=out_names,
+                          dynamic_axes={"input_ids": [0, 1],
+                                        "attention_mask": [0, 1],
+                                        "pred_ids": [0, 1]
+                                        }
+                          )
